59c8197c41c398af420a710d258ad35c27f73133
Danilo0125 <achavezc1@est.emi.edu.bo>
2024-11-12 22:21:20 -0400

    cambio 😊

--- app/models/users.py
+++ app/models/users.py
@@ -1,9 +1,20 @@
 # models.py
 from sqlmodel import SQLModel, Field
+from datetime import date
+from enum import Enum
+
+class UserType(str, Enum):
+    client = "client"
+    admin = "admin"
 
 class UsersBase(SQLModel):
     name: str
-    mail: str = Field(unique=True)
+    last: str
+    email: str = Field(unique=True)
+    phone: str
+    registration_date: date
+    user_type: UserType
+    direccion: str
 
 class Users(UsersBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -11,3 +22,13 @@ class Users(UsersBase, table=True):
 
 class UsersCreate(UsersBase):
     password: str
+
+# user_id:type = int
+# name:type = string
+# last:type = string
+# phone:type = string
+# email:type = string
+# registration_date:type = Date
+# user_type:type = Emun('client', 'admin')
+# direccion:type = string
+# sha256_passwd:type = string
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -5,11 +5,20 @@ from app.security.security import hash_password
 
 # Crear usuario
 def create_user_service(user: UsersCreate, session: Session):
-    db_user = session.exec(select(Users).where(Users.name == user.name))
+    db_user = session.exec(select(Users).where(Users.email == user.email)).first()
     if db_user:
         raise HTTPException(status_code=400, detail="user's email already exist")
     hashed_password = hash_password(user.password)
-    db_user = Users(name=user.name, mail=user.mail, hashed_password=hashed_password)
+    db_user = Users(
+        name=user.name,
+        last=user.last,
+        email=user.email,
+        phone=user.phone,
+        registration_date=user.registration_date,
+        user_type=user.user_type,
+        direccion=user.direccion,
+        hashed_password=hashed_password
+    )
     session.add(db_user)
     session.commit()
     session.refresh(db_user)
@@ -32,7 +41,12 @@ def update_user_service(user_id: int, user_data: UsersCreate, session: Session):
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     user.name = user_data.name
-    user.mail = user_data.mail
+    user.last = user_data.last
+    user.email = user_data.email
+    user.phone = user_data.phone
+    user.registration_date = user_data.registration_date
+    user.user_type = user_data.user_type
+    user.direccion = user_data.direccion
     user.hashed_password = hash_password(user_data.password)
     session.add(user)
     session.commit()
